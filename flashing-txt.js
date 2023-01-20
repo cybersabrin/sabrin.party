88cdae91fd2fbd02c51ebc721e0bb1b7f529449c
sabrin <mailtosabrin@protonmail.ch>
2023-01-20 18:22:26 -0500

    trying to flash text?

---
+++ flashing-txt.js
@@ -0,0 +1,10 @@
+var count_x = 1,
+max_x = 5;
+
+var flash_color_notify = setInterval (function () {
+  if (count_x % 2 === 0 {
+    $('#element').css('color', 'blue');
+  } else {
+    $('#element').css('color', 'yellowgreen');
+  })  
+})
