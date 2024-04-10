6075425b88a4e3b5726aaa75a6adf085ad07bf35
sabrin <mailtosabrin@protonmail.ch>
2024-04-09 21:11:36 -0400

    sdf

--- public/filter.js
+++ public/filter.js
@@ -1,14 +1,34 @@
-var filterButtons = document.querySelectorAll(".btn");
-    filterButtons.forEach(function(button) {
-        button.addEventListener("click", function() {
-            var filterValue = this.getAttribute("data-filter");
-            var reviews = document.getElementsByClassName("log");
-            for (var i = 0; i < reviews.length; i++) {
-                if (filterValue === "all" || reviews[i].classList.contains(filterValue)) {
-                    reviews[i].style.display = "block";
-                } else {
-                    reviews[i].style.display = "none";
-                }
-            }
-        });
-    });
+filterObjects("all");
+
+function filterObjects(c) {
+    var x, i;
+    x = document.getElementsByClassName("log");
+    if (c == "all") c = "";
+    for (i = 0; i < x.length; i++) {
+        removeClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    }
+}
+
+function addClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function removeClass(element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
