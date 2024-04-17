ad6eede7eb9b367de5d65679aeb49c84633abc31
sabrin <mailtosabrin@protonmail.ch>
2024-04-17 00:04:57 -0400

    f

---
+++ code-div.js
@@ -0,0 +1,29 @@
+var SPREADSHEET_ID_AND_TAB = "1e7ePuKiSJho2Q0yhnFx4j0e5fycilkUBE0qNRior4os/log";
+
+$(document).ready(function () {
+  
+    $.getJSON("https://opensheet.elk.sh/" + SPREADSHEET_ID_AND_TAB, function (data) {
+      
+      /* TO VIEW CONSOLE.LOG RESULTS, RIGHT-CLICK AND INSPECT */
+      console.log(data); // This gives me an ARRAY of every OBJECT...
+      
+      data.forEach(function (entry, index) { // For each object in data, read it as an ENTRY...
+        
+        console.log(entry); // This logs the OBJECT...
+        
+        if(index == 0) return;
+        
+        // class is an identifier
+        let div = $(`<div class="item">
+          <h3>` + entry.title +`</h3>
+          <p class="desc" style="font-weight: bold;">` +  entry.type + `</p>
+          <p class="desc">` +  entry.date + `</p>
+          <p class="desc">` +  entry.status + `</p>
+          <p class="cover">` +  entry.image + `</p>
+          <p class="text">` +  entry.review + `</p>
+          </div>`)
+        .appendTo("#content"); // # refers to div id
+    
+      });
+    });  
+  });
