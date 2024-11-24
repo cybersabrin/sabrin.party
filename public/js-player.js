eac3f688a425295f8978a5c1a6feafbe26f56346
sabrin <mailtosabrin@protonmail.ch>
2024-11-24 02:27:13 -0500

    pls work

--- public/js-player.js
+++ public/js-player.js
@@ -1,9 +1,3 @@
-// Possible improvements:
-// - Change timeline and volume slider into input sliders, reskinned
-// - Change into Vue or React component
-// - Be able to grab a custom title instead of "Music Song"
-// - Hover over sliders to see preview of timestamp/volume change
-
 const audioPlayer = document.querySelector(".audio-player");
 const audio = new Audio(
   "/assets/01-home/tunes.mp3"
